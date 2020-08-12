6b4f1afb2b04b83026b8292e2c778650df141480
deepaknailwal <deepaknailwal@192.168.0.102>
2020-08-12 18:11:34 +0530

    Currency Conversion Service

---
+++ CurrencyConversionService/src/main/java/com/microservices/model/CurrencyConversionBean.java
@@ -0,0 +1,73 @@
+package com.microservices.model;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+
+	private long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionFactor;
+	private BigDecimal quantity;
+	private BigDecimal calculatedAmount;
+	private int port;
+		
+	public CurrencyConversionBean() {
+		super();
+	}
+	public CurrencyConversionBean(long id, String from, String to, BigDecimal conversionFactor, BigDecimal quantity,
+			BigDecimal calculatedAmount, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionFactor = conversionFactor;
+		this.quantity = quantity;
+		this.calculatedAmount = calculatedAmount;
+		this.port = port;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public BigDecimal getConversionFactor() {
+		return conversionFactor;
+	}
+	public void setConversionFactor(BigDecimal conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+	public BigDecimal getCalculatedAmount() {
+		return calculatedAmount;
+	}
+	public void setCalculatedAmount(BigDecimal calculatedAmount) {
+		this.calculatedAmount = calculatedAmount;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	
+}
