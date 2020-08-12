cf9b33a9b7d4b83db8d2349cf9a1b41e33dcd002
deepaknailwal <deepaknailwal@192.168.0.102>
2020-08-12 18:13:29 +0530

    Currency Exchange Service

---
+++ CurrencyExchangeService/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+Insert into exchange_value(id,currency_from,currency_to,conversion_factor) values(1001,'USD','INR',65);
+Insert into exchange_value(id,currency_from,currency_to,conversion_factor) values(1002,'EUR','INR',75);
+Insert into exchange_value(id,currency_from,currency_to,conversion_factor) values(1003,'AUD','INR',25);
